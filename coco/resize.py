d63408102c6d02dbd683a7475b7ab135405564fc
abdullahejazjanjua <abdullahejaz822@gmail.com>
2025-11-04 16:39:24 +0500

    Added automatically resizing of all splits

--- coco/resize.py
+++ coco/resize.py
@@ -2,12 +2,14 @@ import os
 import json
 import cv2
 
-def resize_image_and_annotations(image_dir, coco_json_path, output_dir, target_size):
-    
+def resize_image_and_annotations(image_dir, coco_json_path, output_dir, target_size, downscale_factor):
+    """
+        Resizes an image to target_size or downscales it by downscale factor and saves to specified
+        path
+    """
     with open(coco_json_path, 'r') as f:
         coco_data = json.load(f)
 
-    
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -22,6 +24,13 @@ def resize_image_and_annotations(image_dir, coco_json_path, output_dir, target_s
             continue
 
         height, width = image.shape[:2]
+        if target_size:
+            pass
+        elif downscale_factor:
+            target_size = (width //  downscale_factor, height // downscale_factor)
+        else:
+            raise ValueError("Either target size or downscale factor must be provided")
+
         image_resized = cv2.resize(image, target_size)
 
         resized_image_path = os.path.join(output_dir, image_filename)
@@ -46,15 +55,25 @@ def resize_image_and_annotations(image_dir, coco_json_path, output_dir, target_s
         image_info['width'] = target_size[0]
         image_info['height'] = target_size[1]
 
-    output_json_path = os.path.join(output_dir, f'{coco_json_path.split("/")[-1]}_resized.json')
+    base_name = os.path.splitext(os.path.basename(coco_json_path))[0]
+    output_json_path = os.path.join(output_dir, f'{base_name}_resized.json')
     with open(output_json_path, 'w') as f:
         json.dump(coco_data, f)
 
-    print(f"Resized images and annotations saved to {output_dir}")
+    print(f"    Resized images and annotations saved to {output_dir}")
 
-image_directory = 'path/to/images'
-coco_json_path = 'path/to/annotationons.json' 
-output_directory = 'path/to/output' 
 
-resize_image_and_annotations(image_directory, coco_json_path, output_directory, target_size=(640, 640))
+base_image_path = "path/to/image_splits"
+base_ann_path= '/path/to/annotations/'
+output_base_path = '/path/to/output/'
+target_size = (640, 640) # W, H
 
+splits = ["train", "val", "test"]
+
+for split in splits:
+    print(f"Processing {split}...")
+    images_directory_path = os.path.join(base_image_path, split)
+    coco_json_path = os.path.join(base_ann_path, f"{split}.json")
+    output_path = os.path.join(output_base_path, split)
+
+    resize_image_and_annotations(images_directory_path, coco_json_path, output_path, None, 4)
